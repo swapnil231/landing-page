200597499c365f2315f52e5144195e67a96e65a4
swapnil231 <swap200396@gmail.com>
2023-09-13 19:50:57 +0530

    final-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherModule } from './weather/weather.module';
+import { HttpClientModule } from '@angular/common/http';
+import { NotificationModule } from './notification/notification.module';
+import { NewsapiModule } from './newsapi/newsapi.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    WeatherModule,
+    NotificationModule,
+    NewsapiModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
